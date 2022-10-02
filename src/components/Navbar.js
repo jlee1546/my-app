133c9b8c5cd0cba344ee0fea585096c49a226692
Jonathan Lee <jlee1546.jl@gmail.com>
2022-10-02 12:04:42 -0400

    Pushing project to Github

---
+++ src/components/Navbar.js
@@ -0,0 +1,18 @@
+import React from "react";
+
+function Navbar() {
+  return (
+    <nav>
+      <img
+        id="react-icon"
+        src="../logo192.png"
+        alt="React Icon"
+        className="nav--icon"
+      />
+      <h3 className="nav--logo_text">ReactFacts</h3>
+      <h4 className="nav--title">React Course - Project 1</h4>
+    </nav>
+  );
+}
+
+export default Navbar;

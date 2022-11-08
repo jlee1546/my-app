a4baf2ec03d357b47dbfa06c7bb0b249d7f3eb5a
Jonathan Lee <jlee1546.jl@gmail.com>
2022-11-08 12:21:29 -0500

    Pass props to Main component and setup classname

--- src/components/Main.js
+++ src/components/Main.js
@@ -1,8 +1,8 @@
 import React from "react";
 
-function Main() {
+function Main(props) {
   return (
-    <main className="main">
+    <main className={props.darkMode ? "dark" : ""}>
       <h1 className="main--title">Fun facts about React</h1>
       <ul className="main--list">
         <li className="main--list-item">Was first released in 2013</li>
